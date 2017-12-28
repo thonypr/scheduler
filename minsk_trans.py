7dbeee3e8f96549388743a71532609e630eaefc2
a.primako <a.primako@a1qa.com>
2017-12-28 15:56:25 +0300

    get stops

--- minsk_trans.py
+++ minsk_trans.py
@@ -6,37 +6,6 @@ def get_routes_list():
 
     routes_list = []
     csv_raw_routes = requests.get('http://www.minsktrans.by/city/minsk/routes.txt').content.decode("utf-8-sig").encode("utf-8").split("\r\n")
-    del csv_raw_routes[-1]
-
-    for route in csv_raw_routes:
-        route_item = route.split(';')
-        route_dict = {}
-        route_dict['RouteNum'] = route_item[0]
-        route_dict['Authority'] = route_item[1]
-        route_dict['City'] = route_item[2]
-        route_dict['Transport'] = route_item[3]
-        route_dict['Operator'] = route_item[4]
-        route_dict['ValidityPeriods'] = route_item[5]
-        route_dict['SpecialDates'] = route_item[6]
-        route_dict['RouteTag'] = route_item[7]
-        route_dict['RouteType'] = route_item[8]
-        route_dict['Commercial'] = route_item[9]
-        route_dict['RouteName'] = route_item[10]
-        route_dict['Weekdays'] = route_item[11]
-        route_dict['RouteID'] = route_item[12]
-        route_dict['Entry'] = route_item[13]
-        route_dict['RouteStops'] = route_item[14]
-        route_dict['Pikas2012.11.19'] = route_item[15]
-        route_dict['Datestart'] = route_item[16]
-        routes_list.append(route_dict)
-
-    return routes_list
-
-
-def get_stops_list():
-
-    routes_list = []
-    csv_raw_routes = requests.get('http://www.minsktrans.by/city/minsk/routes.txt').content.decode("utf-8-sig").encode("utf-8").split("\r\n")
     print csv_raw_routes[-1]
     del csv_raw_routes[-1]
     headlines = csv_raw_routes[0].split(';')
@@ -89,7 +58,72 @@ def get_stops_list():
             i += 1
         # if i > routes_list.__len__():
         #     break
-    print u'i = {}'.format(i)
-    return routes_list
+    # print u'i = {}'.format(i)
 
-get_stops_list()
+    # for x in filtered_routes:
+    #     print "{}".format(x['RouteNum'])
+
+    return filtered_routes
+
+
+def get_stops_list():
+
+    stops_list = []
+    csv_raw_stops = requests.get('http://www.minsktrans.by/city/minsk/stops.txt').content.decode("utf-8-sig").encode("utf-8").split("\n")
+    print csv_raw_stops[-1]
+    del csv_raw_stops[-1]
+    headlines = csv_raw_stops[0].split(';')
+    del csv_raw_stops[0]
+
+    for stop in csv_raw_stops:
+        stop_item = stop.split(';')
+        stop_dict = {}
+        stop_dict['{}'.format(headlines[0])] = stop_item[0]
+        stop_dict['{}'.format(headlines[1])] = stop_item[1]
+        stop_dict['{}'.format(headlines[2])] = stop_item[2]
+        stop_dict['{}'.format(headlines[3])] = stop_item[3]
+        stop_dict['{}'.format(headlines[4])] = stop_item[4]
+        stop_dict['{}'.format(headlines[5])] = stop_item[5]
+        stop_dict['{}'.format(headlines[6])] = stop_item[6]
+        stop_dict['{}'.format(headlines[7])] = stop_item[7]
+        stop_dict['{}'.format(headlines[8])] = stop_item[8]
+        stop_dict['{}'.format(headlines[9])] = stop_item[9]
+        # stop_dict['{}'.format(headlines[10])] = stop_item[10]
+
+        stops_list.append(stop_dict)
+
+    filtered_stops = []
+#    print stops_list[0]['RouteNum']
+
+    i = 0
+    print stops_list.__len__()
+
+    # while i < stops_list.__len__():
+    #     print "entered"
+    #     print u'i = {}'.format(i)
+    #     если указан RouteNum ок, берём элемент
+        # if stops_list[i]['RouteNum']:
+        #     print "found init"
+            # filtered_stops.append(stops_list[i])
+            # i += 1
+            # проверить, есть ли реверс
+            # if not stops_list[i]['RouteNum'] and stops_list[i]['RouteName'].split(" - ").sort() == stops_list[i-1]['RouteName'].split(" - ").sort():
+            #     print "found reverse"
+                # stops_list[i]['RouteNum'] = stops_list[i-1]['RouteNum']
+                # filtered_stops.append(stops_list[i])
+                # i += 1
+        # else:
+        #     i += 1
+        # if i > stops_list.__len__():
+        #     break
+    # print u'i = {}'.format(i)
+    #
+    # for x in filtered_stops:
+    #     print "{}".format(x['RouteNum'])
+
+    # return filtered_stops
+    return stops_list
+
+# get_routes_list()
+x = get_stops_list()
+print x[456]['Name']
